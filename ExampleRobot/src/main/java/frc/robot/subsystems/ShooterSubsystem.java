b9b6d1c09bc14ac33a871c5b733e7be85b1a9589
mfarag2 <maryhfarag@gmail.com>
2020-01-12 09:41:45 -0500

    Update ShooterSubsystem.java

---
+++ ExampleRobot/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -0,0 +1,27 @@
+public class ShooterSubsystem extends Subsystem {
+    CANSparkMax neo = new CANSparkMax();
+    CANSparkMax topMotor = new CANSparkMax(2, CANSparkMaxLowLevel.MotorType.kBrushless);
+    CANSparkMax bottomMotor = new CANSparkMax (3, CANSparkMaxLowLevel.MotorType.kBrushless);
+
+    public void ShooterUp() {
+        neo.set(1);
+    }
+
+    public void ShooterDown() {
+        neo.set(-1);
+    }
+
+    public void shoot() {
+        // top motor counterclockwise, bottom motor clockwise @ full speed
+        topMotor.set(-1);
+        bottomMotor.set(1);
+    }
+
+    @Override
+
+    public void initDefaultCommand(){
+        // set the default command
+        // setDefaultCommand (new mySpecialCommand());
+    }
+    
+}
