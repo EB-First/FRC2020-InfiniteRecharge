9955eb9f64070e29adcef7df5e254f2a0272c982
fcai254 <53529671+fcai254@users.noreply.github.com>
2020-01-12 10:21:04 -0500

    Update ShooterDownCommand.java

--- ExampleRobot/src/main/java/frc/robot/commands/ShooterDownCommand.java
+++ ExampleRobot/src/main/java/frc/robot/commands/ShooterDownCommand.java
@@ -1,3 +1,10 @@
+package frc.robot.commands;
+
+import javax.annotation.OverridingMethodsMustInvokeSuper;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+
 public class ShooterDownCommand extends Command {
     private ShooterSubsystem m_Shootersystem = Robot.m_ShooterSubsystem;
     public ShooterDownCommand {
@@ -15,7 +22,7 @@ protected void initialize () {
 protected void execute() {
     m_ShooterSubsystem.shooterDown();
 }
- 
+
 @Override
 protected boolean isFinished() {
     return false;
