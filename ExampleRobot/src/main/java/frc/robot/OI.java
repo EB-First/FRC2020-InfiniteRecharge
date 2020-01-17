365286662fae89eb07b1213a65b4cb057b34e836
mfarag2 <maryhfarag@gmail.com>
2020-01-17 15:37:32 -0500

    Create OI.java

---
+++ ExampleRobot/src/main/java/frc/robot/OI.java
@@ -0,0 +1,35 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import frc.robot.variables.RobotMap;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import frc.robot.commands.*;
+import frc.robot.variables.ControlMap;;;
+
+/**
+ * Add your docs here.
+ */
+public class OI {
+    protected GenericHID rightJoystick = new Joystick(RobotMap.operatorGamepad);
+    protected Button shootButton = new JoystickButton(rightJoystick, ControlMap.shootButton);
+
+    public GenericHID getRightJoystick(){ return rightJoystick; }
+
+    public double readRightForwardAxis() { return rightJoystick.getY();}
+
+    public OI() {
+    
+    shootButton.whileHeld(new ShootCommand());
+   
+}
+   
+}
